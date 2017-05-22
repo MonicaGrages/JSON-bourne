d7ccc8fb2e59b2fa3f33e6e5e6cf2ae2e67e4542
MonicaGrages <mgrages1@gmail.com>
2017-05-22 19:50:14 -0400

    put button click events on missions

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 $(function () {
 
-  var ajax = $.get("https://api.myjson.com/bins/1680y")
-  .done (function(data, textStatus, jqXHR) {
-    console.log(ajax.responseJSON.passphrase);
+
+
+  $("#mission_one_btn").on ('click', function () {
+    var ajax = $.get("https://api.myjson.com/bins/1680y")
+    .done (function(data, textStatus, jqXHR) {
+      console.log(ajax.responseJSON.passphrase);
+    });
   });
 
 
-  $.ajax({
-      url:"https://api.myjson.com/bins",
-      type:"POST",
-      data:'{"passphrase":"lemonchiffon"}',
-      contentType:"application/json; charset=utf-8",
-      dataType:"json",
-      success: function(data, textStatus, jqXHR){
-        console.log(data);
-        console.log('successful post');
-      }
+  $("#mission_two_btn").on ('click', function () {
+    $.ajax({
+          url:"https://api.myjson.com/bins",
+          type:"POST",
+          data:'{"passphrase":"lemonchiffon"}',
+          contentType:"application/json; charset=utf-8",
+          dataType:"json",
+          success: function(data, textStatus, jqXHR){
+            console.log(data);
+            console.log('successful post');
+          }
+      });
   });
 
-
+  $("#mission_three_btn").on ('click', function () {
+    ajax = $.get("https://api.myjson.com/bins/3te1c")
+    .done (function(data, textStatus, jqXHR) {
+      console.log(ajax.responseJSON.passphrase);
+    });
+  });
 
 
 });
