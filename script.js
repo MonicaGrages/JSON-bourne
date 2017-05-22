f04f2ad05d32cfe9250911668c5e35b8592818c5
MonicaGrages <mgrages1@gmail.com>
2017-05-22 19:34:01 -0400

    successfully posted passphrase

--- script.js
+++ script.js
@@ -6,7 +6,17 @@ $(function () {
   });
 
 
-
+  $.ajax({
+      url:"https://api.myjson.com/bins",
+      type:"POST",
+      data:'{"passphrase":"lemonchiffon"}',
+      contentType:"application/json; charset=utf-8",
+      dataType:"json",
+      success: function(data, textStatus, jqXHR){
+        console.log(data);
+        console.log('successful post');
+      }
+  });
 
 
 
