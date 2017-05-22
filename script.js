e98e5567f34bdae10b824f172c23f6f548e3fe5f
MonicaGrages <mgrages1@gmail.com>
2017-05-22 19:55:14 -0400

    changed mission status at appropriate points in missions

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ $(function () {
 
 
   $("#mission_one_btn").on ('click', function () {
+    $('#mission_status').html('In Progress');
     var ajax = $.get("https://api.myjson.com/bins/1680y")
     .done (function(data, textStatus, jqXHR) {
       console.log(ajax.responseJSON.passphrase);
+        $('#mission_status').html('Completed');
     });
   });
 
 
   $("#mission_two_btn").on ('click', function () {
+    $('#mission_status').html('In Progress');
     $.ajax({
           url:"https://api.myjson.com/bins",
           type:"POST",
@@ -20,16 +23,21 @@ $(function () {
           success: function(data, textStatus, jqXHR){
             console.log(data);
             console.log('successful post');
+            $('#mission_status').html('Completed');
           }
       });
   });
 
   $("#mission_three_btn").on ('click', function () {
+    $('#mission_status').html('In Progress');
     ajax = $.get("https://api.myjson.com/bins/3te1c")
     .done (function(data, textStatus, jqXHR) {
       console.log(ajax.responseJSON.passphrase);
+      $('#mission_status').html('Completed');
     });
   });
+
+
 
 
 });
